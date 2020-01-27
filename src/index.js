54d1ce2223fb6a2610f1c72726ca4ba786bfcb4b
Gugatharsan <36631649+ezioguga@users.noreply.github.com>
2020-01-27 21:09:41 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+app.get('/users', (req,res) => res.send([]))
+
 
 app.listen(3000,function() {
     console.log("server is running")
